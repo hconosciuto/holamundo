8ecd310a69caea418e7d5b8f33aff156772f0862
Hernan Conosciuto <hconosciuto@gmail.com>
2020-09-07 15:46:17 -0300

    Fix Flask.

--- HolaMundo.py
+++ HolaMundo.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 def hello_www():
 
     return "Hola Mundo, la fecha y hora son: " + vfecha
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)  # pragma: no cover
+
